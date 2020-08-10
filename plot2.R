4ff7bd7dd509e8f84e8cd04756e52ce6b416ea6e
rgrivasp71 <rgrivasp71@gmail.com>
2020-08-10 15:06:39 +0000

    v2

--- plot2.R
+++ plot2.R
@@ -23,7 +23,7 @@ if(!exists("SCC")){
 subsetNEI  <- NEI[NEI$fips=="24510", ]
 aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
 
-# 2.2. Generating plot1.png
+# 2.2. Generating plot2.png
 png('plot2.png')
 barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
 dev.off()
